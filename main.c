9301557da7893632b1da4dd78efdd0e3ae00aac5
Shengyou Jiang <sjj5492@psu.edu>
2020-09-16 18:39:48 +0000

    1st version

--- main.c
+++ main.c
@@ -4,17 +4,18 @@
 #include <math.h>
 
 
-int digit_sum(int n) 
-{ 
-    if (n == 0) 
-       return 0; 
-    return (n % 10 + digit_sum(n / 10)); 
-} 
+int digit_sum(int n) {
+  if (n == 0) {
+    return 0;
+  }
+  else {
+    return (n % 10 + digit_sum(n / 10));
+  }
+}
   
-int main() 
-{   int num = readline("Enter an int: "); 
-    int result = digit_sum(num); 
-    printf("Sum of digits of %d is %d.\n", num, result); 
-    return 0; 
+int main() {   
+  int n = atoi(readline("Enter an int: ")); 
+  int result = digit_sum(n); 
+  printf("Sum of digits of %d is %d.\n", n, result); 
+  return 0; 
 } 
-// sum of digits of 123456789 is 45.
